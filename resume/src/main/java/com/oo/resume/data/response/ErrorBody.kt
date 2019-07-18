3e6d20e9dfbbca392f70b272e05396895dde42f1
yangchao29 <yangchao29@meituan.com>
2019-07-18 21:11:57 +0800

    Init submodule.

--- resume/src/main/java/com/oo/resume/data/response/ErrorBody.kt
+++
@@ -1,9 +0,0 @@
-package com.oo.resume.data.response
-
-/**
- *   yangchao
- *    cd.uestc.superyoung@gmail.com
- *     2019-06-07 11:24
- *
- */
-data class ErrorBody(val code: Int, val msg: String?)
